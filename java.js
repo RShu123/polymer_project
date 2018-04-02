ac59028fe5ac09d3cc8051469761ef8a607a158b
Roan <roan.schalkwijk@student.hu.nl>
2018-04-02 17:30:16 +0200

    veel aanpassingen gemaakt, toevoegen van meerdere functionaliteiten

--- java.js
+++ java.js
@@ -1,27 +1,20 @@
-<script>
-// Get the modal
-var modal = document.getElementById('myModal');
+window.onload = function(){
+var button = document.getElementById("bestelknop");
+var clickBtn = ProductenHeader.shadowRoot.getElementById('testknop');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// Disable the button on initial page load
+button.disabled = true;
+alert(button);
+alert(clickBtn);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//add event listener
+clickBtn.addEventListener('testknop', function(event) {
+    button.disabled = !button.disabled;
+    alert(button);
+});
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+button.addEventListener('testknop', function(event) {
+    alert('Enabled!');
+});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
+};
